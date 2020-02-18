41ff78ad564fc450336cbd20cfa77a393bb39369
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-02-17 22:29:56 -0500

    Continue extending the catalog class and almost complete, learned a bit of iterators and generators and hope to apply some knowledge of that here. In particular will adjust so it can work with a generator as well for now. Hopefully doesn't take too long but good learning experience to learn how to return a modified generator.

--- src/Catalog.py
+++ src/Catalog.py
@@ -3,65 +3,93 @@ from astropy.table import Table
 from astropy.io import ascii
 import numpy as np
 
-# particle mass (Msun), total particles, box size (Mpc/h).
+# particle mass (Msun/h), total particles, box size (Mpc/h).
 catalog_properties = {
     'Bolshoi': (1.35e8, 2048**3, 250),
-    'BolshoiP': (),
-    'MDPL2': ()
+    'BolshoiP': (1.55e8, 2048**3, 250),
+    'MDPL2': (1.51e9, 3840**3, 1000)
 }
 
 
 class HaloCatalog(object):
 
-    # ToDo: Eventually handle chunkification by using a generator.
-    def __init__(self, filename, catalog_type, params=None, filters=None, use_generator=False):
+    def __init__(self, filename, catalog_name, param_names=None, filters=None, use_generator=False):
         """
 
         :param filename:
-        :param catalog_type: Bolshoi / BolshoiP / MDPL2
-        :param params:
+        :param catalog_name: Bolshoi / BolshoiP / MDPL2
+        :param param_names:
         :param filters:
         :param use_generator:
         """
-        assert catalog_type in catalog_properties
+        assert catalog_name in catalog_properties, "Catalog type is not recognized."
 
         # 100 Mb chunks of maximum memory in each iteration.
         # this returns a generator.
         self.filename = filename
-        self.catalog_type = catalog_type
-        self.particle_mass, self.total_particles, self.box_size = catalog_properties[catalog_type]
+        self.catalog_name = catalog_name
+        self.particle_mass, self.total_particles, self.box_size = catalog_properties[self.catalog_name]
+        self.required_params = []
 
         self.use_generator = use_generator
-        self.params = params if params else self.get_default_params()
-        self.filter = filters if filters else self.get_default_filters()
-        self.gcats, self.cat = self.get_cat()
+        self.param_names = param_names if param_names else self.get_default_param_names()
+        self.filters = filters if filters else self.get_default_filters()
 
-        if self.cat:
-            self.cat = self.cat[self.filter(self.cat)]
+        # ToDo: Deal with the fact that filters might contain non native parameters so need to automatically update
+        # with the non-native parameters to account for this.
+        # ToDo: Decide which parameters should be require for catalog? (Do it here or in the dict of parameters)
+        self.required_params.extend(self.filters.keys())
+
+        self.cat = self.get_cat()  # could be a generator or an astropy.Table object.
+
+        # ToDo: Check if this is possible with a generator.
+        assert self.required_params in self.cat.colnames
+
+        if not self.use_generator:
+            self.cat = self.filter_cat(self.cat)
+
+        # ToDo: For the generator case we want to returned a modified generator with the filtered values.
+
+    def get_cat_generator(self):
+        """
+        This will eventually contain all the complexities of Phil's code and return a generator to access
+        the catalog in chunks. For now it offers the chunkified version of my catalog how I've been doing it.
+        :return:
+        """
+        return ascii.read(self.filename, format='csv', guess=False,
+                          fast_reader={'chunk_size': 100 * 1000000, 'chunk_generator': True})
 
     def get_cat(self):
-        gcats = ascii.read(self.filename, format='csv', guess=False,
-                           fast_reader={'chunk_size': 100 * 1000000, 'chunk_generator': True})
+        gcats = self.get_cat_generator()
+
         if self.use_generator:
-            return gcats, None
+            return gcats
 
         else:
-            # actually extract the data from gcats.
+            # actually extract the data from gcats and read it into memory.
             cats = []
             for i, cat in enumerate(gcats):
-                cats.append(cat[self.params])
+                cats.append(cat[self.param_names])
 
-            return None, astropy.table.vstack(cats)
+            return astropy.table.vstack(cats)
 
     def add_param(self, param):
         pass
 
     def get_default_filters(self):
-        return lambda cat: ((np.log10(cat['mvir']) > np.log10(self.particle_mass * 1e3)) &
-                            (cat['Spin'] != 0) & (cat['q'] != 0))
+        return {
+            'mvir': lambda cat: np.log10(cat['mvir']) > np.log10(self.particle_mass * 1e3),
+            'Spin': lambda cat: (cat['Spin'] != 0),
+            'q': lambda cat: cat['q'] != 0
+        }
+
+    def filter_cat(self, cat):
+        for my_filter in self.filters.values():
+            cat = cat[my_filter(cat)]
+        return cat
 
     @staticmethod
-    def get_default_params():
-        pass
+    def get_default_param_names():
+        return ['mvir', 'rvir', ]
 
 
