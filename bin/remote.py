1dbff2b5557dc9364f5cc9d4d556a892fb1738bf
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-10-27 13:10:11 -0400

    remote now uses click for simplicity

--- bin/remote.py
+++ bin/remote.py
@@ -5,20 +5,30 @@ Useful functions for submitting sbatch jobs.
 """
 import subprocess
 import numpy as np
-import argparse
 from pathlib import Path
+import click
 
 
 # ToDo: Implement iterations, make it clear how to use them
+@click.command()
+@click.option("--cmd", required=True, type=str)
+@click.option("--jobname", required=True, type=str)
+@click.option("--jobdir", default="temp/jobs", type=str, show_default=True)
+@click.option("--time", default="01.00", type=str, show_default=True)
+@click.option("--nodes", default=1, type=int, show_default=True)
+@click.option("--ntasks", default=1, type=int, show_default=True)
+@click.option("--cpus-per-task", default=1, type=int, show_default=True)
+@click.option("--mem-per-cpu", default="1GB", type=str, show_default=True)
+@click.option("--iterations", default=1, type=int, show_default=True)
 def run_sbatch_job(
     cmd,
-    job_dir_name,
     job_name,
-    time="01:00",
-    nodes=1,
-    ntasks=1,
-    cpus_per_task=1,
-    mem_per_cpu="1GB",
+    job_dir_name,
+    time,
+    nodes,
+    ntasks,
+    cpus_per_task,
+    mem_per_cpu,
     iterations=1,
 ):
     # prepare files and directories
@@ -51,40 +61,5 @@ def run_sbatch_job(
     subprocess.run(f"sbatch {jobfile.as_posix()}", shell=True)
 
 
-def main(args):
-    run_sbatch_job(
-        args.cmd,
-        args.job_dir,
-        args.job_name,
-        args.time,
-        args.nodes,
-        args.ntasks,
-        args.cpus_per_task,
-        args.mem_per_cpu,
-    )
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Run commands from this /bin/ directory remotely.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument("--cmd", type=str, required=True)
-    parser.add_argument("--job-dir", type=str, required=True)
-    parser.add_argument("--job-name", type=str, required=True)
-
-    parser.add_argument("--time", type=str, default="01:00", help="In hours.")
-    parser.add_argument("--nodes", type=int, default=1)
-    parser.add_argument(
-        "--ntasks",
-        type=int,
-        default=1,
-        help="How many processes do you want to run?",
-    )
-    parser.add_argument(
-        "--cpus-per-task", type=int, default=1, help="How many cpus per process?"
-    )
-    parser.add_argument("--mem-per-cpu", type=str, default="1GB")
-
-    pargs = parser.parse_args()
-    main(pargs)
+    run_sbatch_job()
