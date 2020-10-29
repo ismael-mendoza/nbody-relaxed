7ecefcb5c3ffc9c874ab625c0f03869fbc990d76
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-10-29 14:48:33 -0400

    should now be correct

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,15 +1,16 @@
 ROOT_DIR="/home/imendoza/alcca/nbody-relaxed"
-NAME="m11"
+NAME="m11_2"
 OUTPUT="output_$NAME"
+JOBNAME="job_$NAME"
 M_LOW=11.15
 M_HIGH=11.22
 N=10000
-# M_LOW,M_HIGH = (11.15, 11.22), (12.0,12.2)
+# M_LOW,M_HIGH = (11.15, 11.22), (12.0,12.2), (13, 14)
 
 ################# run pipeline #########################
 ./bin/catalog_pipeline.py --outdir $OUTPUT make-ids --m-low $M_LOW --m-high $M_HIGH \
 --n-haloes $N
 ./bin/catalog_pipeline.py --outdir $OUTPUT make-dmcat
 ./bin/catalog_pipeline.py --outdir $OUTPUT make-subhaloes
-./bin/remote.py --cmd "./bin/catalog_pipeline.py --outdir $OUTPUT make-progenitors &&
-./bin/catalog_pipeline.py --outdir $OUTPUT combine-all" --jobname "job_$NAME"
+./bin/remote.py --cmd "./bin/catalog_pipeline.py --outdir $OUTPUT make-progenitors && \
+./bin/catalog_pipeline.py --outdir $OUTPUT combine-all" --jobname $JOBNAME
