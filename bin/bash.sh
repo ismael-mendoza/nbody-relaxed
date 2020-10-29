de99e8d28c61afaab45201ec46287efcd65906ca
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-10-29 13:44:53 -0400

    now should work

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,18 +1,7 @@
 ROOT_DIR="/home/imendoza/alcca/nbody-relaxed"
 BIN_DIR="$ROOT_DIR/bin"
-
-################# write progenitors #########################
-#./bin/remote.py \
-#--cmd "$BIN_DIR/run_progenitors --write --cpus 10" \
-#--job-dir "$ROOT_DIR/temp" \
-#--job-name "write-progenitors" \
-#--nodes 1 --cpus 10 --ntasks 1 --cpus-per-task 1 --mem-per-cpu '6GB' --time '01:30'
-
-
-################# summarize progenitors #########################
-./bin/remote.py \
---cmd "$BIN_DIR/run_progenitors.py --summarize" \
---job-dir "$ROOT_DIR/temp" \
---job-name "summarize-progs" \
---nodes 1 --cpus 1 --ntasks 1 --cpus-per-task 1 --mem-per-cpu '6GB' --time '01:00'
-
+OUTPUT="output_m11"
+M_LOW=11.3
+M_HIGH=11.6
+################# run pipeline #########################
+./bin/catalog_pipeline --
