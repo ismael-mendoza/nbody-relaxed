998669aa33d61e17697267a44b2d2d44849c17b6
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-10-29 14:31:06 -0400

    finish bash

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,18 +1,15 @@
 ROOT_DIR="/home/imendoza/alcca/nbody-relaxed"
-NAME="m12"
+NAME="m11"
 OUTPUT="output_$NAME"
-M_LOW=12.0
-M_HIGH=12.2
+M_LOW=11.3
+M_HIGH=11.6
 N=10000
-################# run pipeline #########################
-#./bin/catalog_pipeline.py --output-dir $OUTPUT make-ids --m-low $M_LOW --m-high $M_HIGH \
-#--n-haloes $N
-#./bin/catalog_pipeline.py --output-dir $OUTPUT make-dmcat
-#./bin/catalog_pipeline.py --output-dir $OUTPUT make-subhaloes
-./bin/remote.py --cmd "./bin/catalog_pipeline.py --output-dir $OUTPUT make-progenitors" --jobname \
- "job_$NAME"
-
-
-
-
 # M_LOW,M_HIGH = (11.3, 11.6), (12.0,12.2)
+
+################# run pipeline #########################
+./bin/catalog_pipeline.py --output-dir $OUTPUT make-ids --m-low $M_LOW --m-high $M_HIGH \
+--n-haloes $N
+./bin/catalog_pipeline.py --output-dir $OUTPUT make-dmcat
+./bin/catalog_pipeline.py --output-dir $OUTPUT make-subhaloes
+./bin/remote.py --cmd "./bin/catalog_pipeline.py --output-dir $OUTPUT make-progenitors &&
+./bin/catalog_pipeline.py --output-dir $OUTPUT combine-all" --jobname "job_$NAME"
