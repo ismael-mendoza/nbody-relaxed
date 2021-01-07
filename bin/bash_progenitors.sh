b2a36eef4e108d0912f1a656273dc7ff467d5b91
Ismael Mendoza <imendoza@umich.edu>
2021-01-07 10:18:29 -0500

    no need to have tests really

--- bin/bash_progenitors.sh
+++ bin/bash_progenitors.sh
@@ -1,4 +1,6 @@
 NAME="bolshoi_trees"
 JOBNAME="job_$NAME"
+TIME=10:00
 CPUS=10
-./bin/remote.py --cmd "./bin/get_progenitor_file.py --cpus $CPUS" --jobname $JOBNAME --time 10:00 --cpus-per-task $CPUS --mem-per-cpu
+MEM=4GB
+./bin/remote.py --cmd "./bin/get_progenitor_file.py --cpus $CPUS" --jobname $JOBNAME --time $TIME --cpus-per-task $CPUS --mem-per-cpu $MEM
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from os.path import dirname
-from pathlib import Path
-import sys
-
-project_path = Path(dirname(dirname(__file__)))  # path to nbody-relaxed
-sys.path.insert(0, project_path)
