fcbf6076fb09b6f6bce1ad02fbfc74654e7e6603
Ismael Mendoza <imendoza@umich.edu>
2021-04-08 13:03:54 -0400

    quantile transformer seems to work on concentration.

--- relaxed/plot_defaults.py
+++ relaxed/plot_defaults.py
@@ -1,7 +1,5 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
-from cycler import cycler
 
 plt.style.use("seaborn-colorblind")
 
@@ -14,18 +12,18 @@ mpl.rcParams.update(
         "axes.labelsize": 24,
         "axes.titlesize": 28,
         # ticks
-        "xtick.major.size": 5,
-        "xtick.minor.size": 2.5,
+        "xtick.major.size": 10,
+        "xtick.minor.size": 5,
         "xtick.major.width": 0.8,
         "xtick.minor.width": 0.6,
-        "xtick.labelsize": 20,
-        "ytick.major.size": 5,
-        "ytick.minor.size": 2.5,
+        "xtick.labelsize": 22,
+        "ytick.major.size": 10,
+        "ytick.minor.size": 5,
         "ytick.major.width": 0.8,
         "ytick.minor.width": 0.6,
-        "ytick.labelsize": 20,
+        "ytick.labelsize": 22,
         # legend
-        "legend.fontsize": 24,
+        "legend.fontsize": 22,
     }
 )
 plt.rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
