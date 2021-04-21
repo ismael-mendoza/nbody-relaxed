7c803bd64bbaea5c8230460b76a4e4b344157d38
Ismael Mendoza <imendoza@umich.edu>
2021-04-21 14:27:43 -0400

    analysis is now more general in terms of the features used

--- relaxed/analysis.py
+++ relaxed/analysis.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from relaxed.halo_parameters import X
 
 import numpy as np
 from astropy.cosmology import LambdaCDM
@@ -32,70 +33,6 @@ def setup(name="m11", path="../../temp"):
     hcat.load_cat_csv()
 
     return hcat, indices, scales
-
-
-def get_m_a_corrs(cat, param, indices):
-    corrs = []
-    for k in indices:
-        k = int(k)
-        colname = f"mvir_a{k}"
-        keep = (~np.isnan(cat[colname])) & (cat[colname] > 0)
-
-        # get mass fraction at this scale
-        mvir = cat["mvir"][keep]
-        ms = cat[colname][keep]
-        ms = ms / mvir
-        pvalue = cat[param][keep]
-
-        # get correlation.
-        assert np.all(ms > 0) and np.all(~np.isnan(ms))
-        assert np.all(mvir > 0)
-        corr = stats.spearmanr(ms, pvalue)[0]
-        corrs.append(corr)
-
-    return np.array(corrs)
-
-
-def get_am_corrs(cat, param, am):
-    corrs = []
-    n_mass_bins = am.shape[1]
-    for k in range(n_mass_bins):
-        corrs.append(stats.spearmanr(cat[param], am[:, k], nan_policy="omit")[0])
-    return np.array(corrs)
-
-
-def add_box_indices(cat, boxes=8, box_size=250):
-    # create a new row add it to the catalogu for which box it is in.
-    assert int(boxes ** (1.0 / 3)) == boxes ** (1.0 / 3)
-    box_per_dim = int(boxes ** (1.0 / 3))
-    divides = np.linspace(0, box_size, box_per_dim + 1)[1:-1]  # only use the middle.
-    cat.add_column(np.zeros(len(cat)), name="ibox")
-    for k, dim in enumerate(["x", "y", "z"]):
-        for d in divides:
-            cat["ibox"] += 2 ** k * (d < cat[dim])
-
-
-def vol_jacknife_values(f, cat, param, *args):
-    # assumes cat has had its box indices added with the function above.
-    n_boxes = int(np.max(cat["ibox"]) + 1)
-    values = []
-    for b in range(n_boxes):
-        _cat = cat[cat["ibox"] != b]
-        value = f(_cat, param, *args)
-        values.append(value)
-    return np.array(values)
-
-
-# create function that converts scale to fractional tdyn
-def get_fractional_tdyn(scale, tdyn, sim_name="Bolshoi"):
-    sim = halo_catalogs.sims[sim_name]
-
-    # get cosmology based on given sim
-    cosmo = LambdaCDM(H0=sim.h * 100, Ob0=sim.omega_b, Ode0=sim.omega_lambda, Om0=sim.omega_m)
-
-    # tdyn in Gyrs
-    z = (1 / scale) - 1
-    return (cosmo.age(0).value - cosmo.age(z).value) / tdyn
 
 
 def get_ma(cat, indices):
@@ -166,14 +103,68 @@ def get_am(name="m11", min_mass=0.1, path="../../temp"):
     return am, np.exp(mass_bins)
 
 
-def get_a2_from_cat(cat, scales, indices):
-    ma = get_ma(cat, indices)
+def get_ma_corrs(cat, param, indices):
+    corrs = []
+    for k in indices:
+        k = int(k)
+        colname = f"mvir_a{k}"
+        keep = (~np.isnan(cat[colname])) & (cat[colname] > 0)
 
-    # obtain a_1/2 corresponding indices
-    idx = np.argmax(np.where(ma < 0.5, ma, -np.inf), 1)
+        # get mass fraction at this scale
+        mvir = cat["mvir"][keep]
+        ms = cat[colname][keep]
+        ms = ms / mvir
+        pvalue = cat[param][keep]
 
-    # and the scales
-    return scales[idx]
+        # get correlation.
+        assert np.all(ms > 0) and np.all(~np.isnan(ms))
+        assert np.all(mvir > 0)
+        corr = stats.spearmanr(ms, pvalue)[0]
+        corrs.append(corr)
+
+    return np.array(corrs)
+
+
+def get_am_corrs(cat, param, am):
+    corrs = []
+    n_mass_bins = am.shape[1]
+    for k in range(n_mass_bins):
+        corrs.append(stats.spearmanr(cat[param], am[:, k], nan_policy="omit")[0])
+    return np.array(corrs)
+
+
+def add_box_indices(cat, boxes=8, box_size=250):
+    # create a new row add it to the catalogu for which box it is in.
+    assert int(boxes ** (1.0 / 3)) == boxes ** (1.0 / 3)
+    box_per_dim = int(boxes ** (1.0 / 3))
+    divides = np.linspace(0, box_size, box_per_dim + 1)[1:-1]  # only use the middle.
+    cat.add_column(np.zeros(len(cat)), name="ibox")
+    for k, dim in enumerate(["x", "y", "z"]):
+        for d in divides:
+            cat["ibox"] += 2 ** k * (d < cat[dim])
+
+
+def vol_jacknife_values(f, cat, param, *args):
+    # assumes cat has had its box indices added with the function above.
+    n_boxes = int(np.max(cat["ibox"]) + 1)
+    values = []
+    for b in range(n_boxes):
+        _cat = cat[cat["ibox"] != b]
+        value = f(_cat, param, *args)
+        values.append(value)
+    return np.array(values)
+
+
+# create function that converts scale to fractional tdyn
+def get_fractional_tdyn(scale, tdyn, sim_name="Bolshoi"):
+    sim = halo_catalogs.sims[sim_name]
+
+    # get cosmology based on given sim
+    cosmo = LambdaCDM(H0=sim.h * 100, Ob0=sim.omega_b, Ode0=sim.omega_lambda, Om0=sim.omega_m)
+
+    # tdyn in Gyrs
+    z = (1 / scale) - 1
+    return (cosmo.age(0).value - cosmo.age(z).value) / tdyn
 
 
 def get_a2_from_am(am, mass_bins):
@@ -181,31 +172,46 @@ def get_a2_from_am(am, mass_bins):
     return am[:, idx]
 
 
+def get_lam(am, *args):
+    # log(a(m)) whilst removing nan's if present.
+    # the mask is applied to each of the extra arguments in `args` for convenience.
+    lam = np.log(am)
+    keep = np.ones(len(lam), dtype=bool)
+    for i in range(len(lam)):
+        keep[i] = ~np.any(np.isnan(lam[i, :]))
+
+    assert np.sum(np.isnan(lam[keep])) == 0
+    return keep, lam[keep], *(arg[keep] for arg in args)
+
+
 def get_quantiles(arr):
     return np.vectorize(lambda x: stats.percentileofscore(arr, x))(arr) / 100.0
 
 
-def gaussian_conditional(x, lam, ind=False):
-    # x represents one of the dark matter halo properties at z=0.
-    # x and log(am) is assumed to be Gaussian.
+def gaussian_conditional(x, y):
+    # y (usually) represents one of the dark matter halo properties at z=0.
+    # x are the features used for prediction, should have shape (y.shape[0], n_features)
 
-    n_bins = lam.shape[1]
-    assert len(x.shape) == 1
-    assert lam.shape == (x.shape[0], n_bins)
+    assert len(y.shape) == 1 and len(x.shape) == 2
+    assert x.shape[0] == y.shape[0]
+    n_features = x.shape[1]
 
-    # calculate sigma/correlation matrix bewteen all quantitie
-    z = np.vstack([x.reshape(1, -1), lam.T]).T
-    assert z.shape == (x.shape[0], n_bins + 1)
-    np.testing.assert_equal(x, z[:, 0])
-    np.testing.assert_equal(lam[:, 0], z[:, 1])  # ignore mutual nan's
-    np.testing.assert_equal(lam[:, -1], z[:, -1])
+    # calculate sigma/correlation matrix bewteen all quantities
+    z = np.vstack([y.reshape(1, -1), x.T]).T
+
+    # some sanity checks
+    assert z.shape == (y.shape[0], n_features + 1)
+    np.testing.assert_equal(y, z[:, 0])
+    np.testing.assert_equal(x[:, 0], z[:, 1])  # ignore mutual nan's
+    np.testing.assert_equal(x[:, -1], z[:, -1])
 
     # calculate covariances
-    Sigma = np.zeros((1 + n_bins, 1 + n_bins))
-    rho = np.zeros((1 + n_bins, 1 + n_bins))
-    for i in range(n_bins + 1):
-        for j in range(n_bins + 1):
+    Sigma = np.zeros((1 + n_features, 1 + n_features))
+    rho = np.zeros((1 + n_features, 1 + n_features))
+    for i in range(n_features + 1):
+        for j in range(n_features + 1):
             if i <= j:
+                # calculate correlation coefficient keepin only non-nan values
                 z1, z2 = z[:, i], z[:, j]
                 keep = ~np.isnan(z1) & ~np.isnan(z2)
                 cov = np.cov(z1[keep], z2[keep])
@@ -216,93 +222,153 @@ def gaussian_conditional(x, lam, ind=False):
                 rho[i, j] = rho[j, i]
                 Sigma[i, j] = Sigma[j, i]
 
+    # more sanity
+    assert np.all(~np.isnan(Sigma))
+    assert np.all(~np.isnan(rho))
+
     # we assume a multivariate-gaussian distribution P(X, a(m1), a(m2), ...) with
     # conditional distribution P(X | {a(m_i)}) uses the rule here:
     # https://stats.stackexchange.com/questions/30588/deriving-the-conditional-distributions-of-a-multivariate-normal-distribution
     # we return the mean/std deviation of the conditional gaussian.
-    assert np.all(~np.isnan(Sigma))
-    assert np.all(~np.isnan(rho))
-
-    mu1 = np.nanmean(z[:, 0]).reshape(1, 1)
-    mu2 = np.nanmean(lam, axis=0).reshape(n_bins, 1)
+    mu1 = np.nanmean(y).reshape(1, 1)
+    mu2 = np.nanmean(x, axis=0).reshape(n_features, 1)
     Sigma11 = Sigma[0, 0].reshape(1, 1)
-    Sigma12 = Sigma[0, 1:].reshape(1, n_bins)
-    Sigma22 = Sigma[1:, 1:].reshape(n_bins, n_bins)
+    Sigma12 = Sigma[0, 1:].reshape(1, n_features)
+    Sigma22 = Sigma[1:, 1:].reshape(n_features, n_features)
 
-    if ind:
-        for i in range(Sigma22.shape[0]):
-            for j in range(Sigma22.shape[1]):
-                if i != j:
-                    Sigma22[i, j] = 0
-
-    def mu_cond(lam_test):
-        assert np.sum(np.isnan(lam_test)) == 0
-        lam_test = lam_test.reshape(-1, n_bins).T
-        mu_cond = mu1 + Sigma12.dot(np.linalg.inv(Sigma22)).dot(lam_test - mu2)
+    def mu_cond(x_test):
+        # computes mu(y | X=x_test)
+        assert np.sum(np.isnan(x_test)) == 0
+        x_test = x_test.reshape(-1, n_features).T
+        mu_cond = mu1 + Sigma12.dot(np.linalg.inv(Sigma22)).dot(x_test - mu2)
         return mu_cond.reshape(-1)
 
     sigma_cond = Sigma11 - Sigma12.dot(np.linalg.inv(Sigma22)).dot(Sigma12.T)
-    return mu1, mu2, Sigma, rho, mu_cond, sigma_cond
+    return {
+        "mu1": mu1,
+        "mu2": mu2,
+        "Sigma": Sigma,
+        "rho": rho,
+        "mu_cond": mu_cond,
+        "sigma_cond": sigma_cond,
+    }
 
 
-def get_lam(am, *args):
-    lam = np.log(am)
-    keep = np.ones(len(lam), dtype=bool)
-    for i in range(len(lam)):
-        keep[i] = ~np.any(np.isnan(lam[i, :]))
-
-    assert np.sum(np.isnan(lam[keep])) == 0
-    return keep, lam[keep], *(arg[keep] for arg in args)
-
-
-def training_suite(Y_train, am_train, mass_bins=None, suite=("MV-LLR", "LN-RS", "CAM")):
+def training_suite(x, y, suite=("LN-RS",), extra_args: dict = None):
     """
-
-    Y_train is raw variable to be predicted (e.g. cvir, xoff, or eta) without logs.
+    Args:
+        x (np.array): (Unlogged) Training data array with features and shape (n_points, n_features)
+        y (np.array): (Unlogged) Training data array with variable to be predicted
+            (e.g. cvir, xoff, or eta)
 
     Legend:
-        - MG-FC: Multi-Variate Gaussian using full covariance matrix. (returns conditional mean)
         - LN-RS: LogNormal random samples.
+        - MG-LFC: Multi-Variate Gaussian using full covariance matrix. (returns conditional mean)
+            with logged variables.
         - CAM: Conditional Abundance Matching
         - MG-A2: Bivariate Gaussian only usig a_{1/2} for conditional estimate.
+        - MV-LR: Linear regression with no logs.
         - MV-LLR: Multi-Variate Linear Regression with logs
-        - MV-LR: Linear regressoin with no logs.
         - MG-TFC: Multi-Gaussian after transforming (non-log) variables with quantile transformer.
+        - MV-TLR: Linear regression after transforming variables with a quantile transformer.
     """
-    assert set(suite).issubset({"MG-FC", "LN-RS", "CAM", "MG-A2", "MV-LLR", "MV-LR", "MG-TFC"})
+    assert set(suite).issubset(
+        {"MG-LFC", "LN-RS", "CAM", "MG-A2", "MV-LLR", "MV-LR", "MG-TFC", "MV-TLR"}
+    )
 
-    lam_train = np.log(am_train)
-    assert np.isnan(lam_train).sum() == 0
+    assert np.isnan(np.log(x)).sum() == 0
+    assert np.isnan(np.log(y)).sum() == 0
 
     trained_models = {}
 
-    if "MG-FC" in suite:
+    # whether using suites that require using a gaussian remapping transformation.
+    if "MV-TLR" in suite or "MG-TFC" in suite:
+        from sklearn.preprocessing import QuantileTransformer
 
-        # multivariate prediction
-        _, _, _, _, mu_cond, _ = gaussian_conditional(np.log(Y_train), lam_train)
+        qt_y = QuantileTransformer(n_quantiles=len(y), output_distribution="normal").fit(
+            y.reshape(-1, 1)
+        )
+        qt_x = QuantileTransformer(n_quantiles=len(x), output_distribution="normal").fit(x)
 
-        def multi_gauss(lam_test):
-            return np.exp(mu_cond(lam_test))
-
-        trained_models["MG-FC"] = multi_gauss
+        y_trans = qt_y.transform(y.reshape(-1, 1)).reshape(-1)
+        x_trans = qt_x.transform(x)
 
     if "LN-RS" in suite:
-        mu, sigma = np.mean(np.log(Y_train)), np.std(np.log(Y_train))
+        mu, sigma = np.mean(np.log(y)), np.std(np.log(y))
 
-        def lognormal(lam_test):
-            n_test = len(lam_test)
+        def lognormal(x_test):
+            n_test = len(x_test)
             log_Y_pred = np.random.normal(mu, sigma, n_test)
             return np.exp(log_Y_pred)
 
         trained_models["LN-RS"] = lognormal
 
+    if "MG-LFC" in suite:
+
+        # multivariate prediction
+        gcond = gaussian_conditional(np.log(x), np.log(y))
+
+        def multi_gauss(x_test):
+            return np.exp(gcond["mu_cond"](np.log(x_test)))
+
+        trained_models["MG-LFC"] = multi_gauss
+
+    if "MG-TFC" in suite:
+        gcond = gaussian_conditional(x_trans, y_trans)
+
+        def multi_gauss_trans(x_test):
+            x_trans_test = qt_am.transform(x_test)
+            y_trans_pred = gcond["mu_cond"](x_trans_test)
+            y_pred = qt_y.inverse_transform(y_trans_pred.reshape(-1, 1))
+            return y_pred.reshape(-1)
+
+        trained_models["MG-TFC"] = multi_gauss_trans
+
+    if "MV-LLR" in suite:
+        # linear regression with logs.
+        # requires shape (n_samples, n_features)
+        from sklearn.linear_model import LinearRegression
+
+        reg1 = LinearRegression().fit(np.log(x), np.log(y))
+
+        def linreg(x_test):
+            return np.exp(reg1.predict(np.log(x_test)))
+
+        trained_models["MV-LLR"] = linreg
+
+    if "MV-LR" in suite:
+        # linear regression (no logs)
+
+        reg2 = LinearRegression().fit(x, y)
+
+        def linreg_no_logs(x_test):
+            return reg2.predict(x_test)
+
+        trained_models["MV-LR"] = linreg_no_logs
+
+    if "MV-TLR" in suite:
+        # linear regression (no logs)
+
+        reg3 = LinearRegression().fit(x_trans, y_trans)
+
+        def linreg_trans(x_test):
+            x_trans_test = qt_x.transform(x_test)
+            y_trans_pred = reg3.predict(x_trans_test)
+            y_pred = qt_y.inverse_transform(y_trans_pred.reshape(-1, 1))
+            return y_pred.reshape(-1)
+
+        trained_models["MV-TLR"] = linreg_trans
+
     if "CAM" in suite:
-        assert mass_bins is not None
+        assert NotImplementedError()
+        assert "mass_bins" in extra_args and "am_train" in extra_args
         from scipy.interpolate import interp1d
+
+        mass_bins, am_train = extra_args["mass_bins"], extra_args["am_train"]
 
         a2_train = get_a2_from_am(am_train, mass_bins)
 
-        Y_sort, a2_sort = -np.sort(-Y_train), np.sort(a2_train)
+        Y_sort, a2_sort = -np.sort(-y), np.sort(a2_train)
         marks = np.arange(len(Y_sort)) / len(Y_sort)
         marks += (marks[1] - marks[0]) / 2
         a2_to_mark = interp1d(a2_sort, marks, fill_value=(0, 1), bounds_error=False)
@@ -315,6 +381,7 @@ def training_suite(Y_train, am_train, mass_bins=None, suite=("MV-LLR", "LN-RS", 
         trained_models["CAM"] = cam
 
     if "MG-A2" in suite:
+        assert NotImplementedError()
 
         # multi-normal but just using a_{1/2}
         indx = np.where((0.498 < mass_bins) & (mass_bins < 0.51))[0].item()
@@ -326,53 +393,5 @@ def training_suite(Y_train, am_train, mass_bins=None, suite=("MV-LLR", "LN-RS", 
             return np.exp(mu_cond_a2(lam_test[:, indx].reshape(-1, 1)))
 
         trained_models["MG-A2"] = a2_gauss
-
-    if "MV-LLR" in suite:
-        # linear regression with logs.
-        # requires shape (n_samples, n_features)
-        from sklearn.linear_model import LinearRegression
-
-        reg1 = LinearRegression().fit(lam_train, np.log(Y_train))
-
-        def linreg(lam_test):
-            return np.exp(reg1.predict(lam_test))
-
-        trained_models["MV-LLR"] = linreg
-
-    if "MV-LR" in suite:
-        # linear regression (no logs)
-
-        reg2 = LinearRegression().fit(am_train, Y_train)
-
-        def linreg_no_logs(lam_test):
-            return reg2.predict(np.exp(lam_test))
-
-        trained_models["MV-LR"] = linreg_no_logs
-
-    if "MG-TFC" in suite:
-
-        # gaussian remapping
-        from sklearn.preprocessing import QuantileTransformer
-
-        qt_Y = QuantileTransformer(n_quantiles=len(Y_train), output_distribution="normal").fit(
-            Y_train.reshape(-1, 1)
-        )
-        qt_am = QuantileTransformer(n_quantiles=len(am_train), output_distribution="normal").fit(
-            am_train
-        )
-
-        Y_trans_train = qt_Y.transform(Y_train.reshape(-1, 1))
-        am_trans_train = qt_am.transform(am_train)
-        _, _, _, _, mu_cond_trans, _ = gaussian_conditional(
-            Y_trans_train.reshape(-1), am_trans_train
-        )
-
-        def multi_gauss_trans(lam_test):
-            am_trans_test = qt_am.transform(np.exp(lam_test))
-            Y_trans_pred = mu_cond_trans(am_trans_test)
-            Y_pred = qt_Y.inverse_transform(Y_trans_pred.reshape(-1, 1))
-            return Y_pred.reshape(-1)
-
-        trained_models["MG-TFC"] = multi_gauss_trans
 
     return trained_models
