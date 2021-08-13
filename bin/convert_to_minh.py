197bff7a955ff3f67b2bffdd4268849fd23eda1f
Ismael Mendoza <imendoza@umich.edu>
2021-08-13 10:23:06 -0500

    utility files for converting files to minh format using minnow

---
+++ bin/convert_to_minh.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import subprocess
+from pathlib import Path
+import multiprocessing as mp
+
+# NOTE: Need to check that *.config file has correct path to `name_index` and `type_index`
+repo = Path("/home/imendoza/nbody-relaxed/")
+executable = "/home/imendoza/minnow/scripts/text_to_minh"
+config = repo.joinpath("params/Bolshoi.config")
+all_vars = repo.joinpath("params/all_vars.txt")
+catalogs_dir = repo.joinpath("data/bolshoi_catalogs")
+output_dir = repo.joinpath("data/bolshoi_catalogs_minh")
+
+
+def work(cat_file):
+    unzipped_file = cat_file.with_suffix("")
+
+    # first convert using gzip (without deleting original)
+    if not unzipped_file.exists():
+        subprocess.run(f"gunzip -c {cat_file} >{unzipped_file}", shell=True)
+
+    # then we run minnow to conver tit to minh.
+    cmd = f"{executable} {config} {all_vars} {unzipped_file} {output_dir}"
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+
+
+cpus = 5
+
+
+cat_files = []
+for cat_file in catalogs_dir.iterdir():
+    if cat_file.suffix == ".gz":
+        output_file = output_dir.joinpath(cat_file.stem).with_suffix(".minh")
+        if not output_file.exists():
+            cat_files.append(cat_file)
+
+with mp.Pool(cpus) as pool:
+    pool.map(work, cat_files)
