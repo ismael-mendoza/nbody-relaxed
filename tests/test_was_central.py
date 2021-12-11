3e4a211b86391a55d947c7099ba9a031444db59a
Ismael Mendoza <imendoza@umich.edu>
2021-12-11 15:37:52 -0600

    comment

--- tests/test_was_central.py
+++ tests/test_was_central.py
@@ -10,5 +10,7 @@ def test_was_central():
     curr_dfids = np.array([9, 10, 14, 1, 44, 4, 27, 46, 46])
     curr_pids = np.array([-1, -1, 8, -1, 8, 11, -1, 7, 7])
 
+    # sub_keep should be True only for haloes that are subhaloes, were centrals in previous snapshot
+    # and have a progenitor in the previous snapshot.
     sub_keep = get_central_subhaloes(prev_pids, prev_dfids, curr_ids, curr_pids, curr_dfids)
     np.testing.assert_equal(sub_keep, np.array([0, 0, 1, 0, 0, 1, 0, 1, 1]).astype(bool))
