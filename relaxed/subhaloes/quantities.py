bdd96155364b62ae77a09596556f5d872dba3b3e
Ismael Mendoza <imendoza@umich.edu>
2021-12-11 15:35:48 -0600

    no test in files but rather in tests/

--- relaxed/subhaloes/quantities.py
+++ relaxed/subhaloes/quantities.py
@@ -103,38 +103,3 @@ def mass_gap(host_mvir, host_ids, sub_mvir, sub_pids):
         sub_index = bins[i]
         mgap[i] = host_mvir[i] - np.max(sub_mvir[sub_index])
     return mgap
-
-
-def run_tests():
-    host_ids = np.array([100, 200, 50, -17], dtype=int)
-    sub_pids = np.array([100, -17, 100, 200, -17, 100, 75, 300, -20], dtype=int)
-    sub_mvir = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=float)
-
-    # test LookupTable
-
-    table = LookupTable(host_ids)
-    sub_index = table.lookup(sub_pids)
-
-    assert np.all(sub_index == np.array([0, 3, 0, 1, 3, 0, -1, -1, -1]))
-
-    # test bin_by_host
-
-    bins = bin_by_host(host_ids, sub_pids)
-
-    assert len(bins) == 4
-    assert np.all(bins[0] == np.array([0, 2, 5]))
-    assert np.all(bins[1] == np.array([3]))
-    assert len(bins[2]) == 0
-    assert np.all(bins[3] == np.array([1, 4]))
-
-    # test M_sub
-
-    M_sub = m_sub(host_ids, sub_mvir, sub_pids)
-
-    assert np.all(M_sub == np.array([10, 4, 0, 7]))
-
-    print("Test passes!")
-
-
-if __name__ == "__main__":
-    run_tests()
