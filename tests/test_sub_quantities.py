8b89b48885168bef33da65fd10070eb2d0f75932
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2021-12-12 07:44:05 -0600

    Merger ratios like rockstar (#64)
    * order from early to late
    
    * unnecessary comment
    
    * simplify code
    
    * order from early to late, also new way of extracting subhalo info consistent with ROCKSTAR
    
    * most massive progenitor is picked automatically with depth first search ID
    
    * we want coprog mvir not ratio so that we can use the previous time step for mergers. load minh files better as suggested by phil
    
    * newest plots with newest data on merger ratios
    
    * newest data processed with subhalo info
    
    * latest plots
    
    * output will not be saved
    
    * change name
    
    * sorry
    
    * delete
    
    * update poetry
    
    * pin version
    
    * small correction
    
    * new test for was_central
    
    * refactor and logging
    
    * add test for phil functions on subhaloes with threshold
    
    * no test in files but rather in tests/
    
    * don't need prev_pids here
    
    * improve logging
    
    * comment
    
    * very confusing results but now it agrees 100 % with rockstar :shrug:

---
+++ tests/test_sub_quantities.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from relaxed.subhaloes.quantities import bin_by_host
+from relaxed.subhaloes.quantities import LookupTable
+from relaxed.subhaloes.quantities import m2_sub
+from relaxed.subhaloes.quantities import m_sub
+
+
+def test_sub_quantities():
+    host_mvir = np.array([10, 5, 10, 8])
+    host_ids = np.array([100, 200, 50, -17], dtype=int)
+    sub_pids = np.array([100, -17, 100, 200, -17, 100, 75, 300, -20], dtype=int)
+    sub_mvir = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=float)
+
+    # test LookupTable
+
+    table = LookupTable(host_ids)
+    sub_index = table.lookup(sub_pids)
+
+    assert np.all(sub_index == np.array([0, 3, 0, 1, 3, 0, -1, -1, -1]))
+
+    # test bin_by_host
+
+    bins = bin_by_host(host_ids, sub_pids)
+
+    assert len(bins) == 4
+    assert np.all(bins[0] == np.array([0, 2, 5]))
+    assert np.all(bins[1] == np.array([3]))
+    assert len(bins[2]) == 0
+    assert np.all(bins[3] == np.array([1, 4]))
+
+    # test M_sub
+
+    M_sub = m_sub(host_ids, host_mvir, sub_pids, sub_mvir, threshold=1 / 4)
+    m2 = m2_sub(host_ids, sub_pids, sub_mvir)
+
+    np.testing.assert_equal(M_sub, np.array([9, 4, 0, 5]))
+    np.testing.assert_equal(m2, np.array([6, 4, 0, 5]))
