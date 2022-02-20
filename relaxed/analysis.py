e6e83ef6405d96591ae8ec6bd703623e0c28aa89
Ismael Mendoza <imendoza@umich.edu>
2022-02-20 10:38:46 -0500

    create functoin a_from_t

--- relaxed/analysis.py
+++ relaxed/analysis.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import findiff
 import numpy as np
 from astropy.cosmology import LambdaCDM
+from astropy.cosmology import z_at_value
 from lmfit import minimize
 from lmfit import Parameters
 from scipy import stats
@@ -298,6 +299,15 @@ def get_fractional_tdyn(scale, tdyn, sim_name="Bolshoi"):
     # tdyn in Gyrs
     z = (1 / scale) - 1
     return (cosmo.age(0).value - cosmo.age(z).value) / tdyn
+
+
+def get_a_from_t(t, sim_name="Bolshoi"):
+    sim = halo_catalogs.sims[sim_name]
+
+    # get cosmology based on given sim
+    cosmo = LambdaCDM(H0=sim.h * 100, Ob0=sim.omega_b, Ode0=sim.omega_lambda, Om0=sim.omega_m)
+    z = z_at_value(cosmo.age, t)  # t in Gyrs
+    return 1 / (1 + z)
 
 
 def get_t_from_a(scale, sim_name="Bolshoi"):
